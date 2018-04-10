ed6c8fcb714a9235d834746aa34905dbf894623c
张钌 <zhangliao@baiwang.com>
2018-04-10 16:39:51 +0800

    add bug 复现代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+
+// this piece of code works
+// const _import = (relativePath) => {
+//   if (process.env.NODE_ENV === 'development') {
+//     return require('@/components/' + relativePath).default
+//   } else {
+//     return import('@/components/' + relativePath)
+//   }
+// };
+
+// the following code does not work
+// Uncaught Error: Cannot find module "."
+const _import = (relativePath) => {
+  const fullRelativePath = '@/components/' + relativePath;
+  if (process.env.NODE_ENV === 'development') {
+    return require(fullRelativePath).default
+  } else {
+    return import(fullRelativePath)
+  }
+};
+
+
+const HelloWorld = _import('HelloWorld');
+
 
 export default new Router({
   routes: [
